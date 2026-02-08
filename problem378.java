7b55af90dcff641b75da203d54575af84fc743a3
Kajalnagar <nagarkajal420@gmail.com>
2026-02-08 13:35:49 +0530

    Create problem378.java

---
+++ problem378.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int[] arr = new int[n * n];
+        int idx = 0;
+
+        // 2D array to 1D array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[idx++] = matrix[i][j];
+            }
+        }
+
+        // Sort
+        Arrays.sort(arr);
+
+        // kth smallest
+        return arr[k - 1];
+    }
+}
