edea0b1396b68ad6ba82b76bd61b0faa06d36158
Kajalnagar <nagarkajal420@gmail.com>
2026-02-09 20:22:39 +0530

    Add rotate method to rotate array elements
    Implement array rotation using reversal algorithm.

---
+++ problem189.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+      int n = nums.length;
+        k = k % n;   // handle k > n
+
+        // Step 1: reverse whole array
+        reverse(nums, 0, n - 1);
+
+        // Step 2: reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 3: reverse remaining elements
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
